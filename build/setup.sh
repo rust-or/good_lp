ff2144dbf2eefdb7e3ddcc72e873f077251fe77b
KnorpelSenf <shtrog@gmail.com>
2024-12-26 11:16:26 +0000

    chore: add system setup script

---
+++ build/setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Print commands, exit upon error
+set -ex
+
+# Change CWD to script location
+cd "${0%/*}"
+
+# Install CBC, HiGHS, lp_solve
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y coinor-cbc coinor-libcbc-dev libgsl-dev build-essential cmake clang
+
+# Install CPLEX
+curl -LO https://github.com/rust-or/good_lp/releases/download/cplex/cplex.bin
+chmod u+x cplex.bin
+./cplex.bin -f ./response.properties
+
+# Install wasm-pack
+cargo install wasm-pack
